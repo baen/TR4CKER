fd134d02674a997ba427c068e34e35fa5943715e
baen <benjamin.halvorson@education.nsw.gov.au>
2025-06-23 10:25:30 +1000

    Adding supabase
    Changed some simple things around and began working on my database then realising that Im gonna use supabase.

---
+++ .database/queries.sql
@@ -0,0 +1,7 @@
+-- CREATE TABLE ExercisesList (id INTEGER PRIMARY KEY AUTOINCREMENT,
+--                exercisename TEXT UNIQUE NOT NULL);
+
+-- CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT,
+--                    username TEXT UNIQUE NOT NULL,
+--                    password TEXT NOT NULL);
+ADD 
