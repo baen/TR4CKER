c1bc67dfae046ab361e0a15104c7b0ed402a447f
baen <benjamin.halvorson@education.nsw.gov.au>
2025-07-02 20:42:51 +1000

    Finished base pages
    The base layout for the pages is complete. As of now everything functions as intended backend work begins.

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 import db
 
 app = Flask(__name__)
 app.secret_key = "gtg"
 
 
+@app.route("/")
+def index():
+    return render_template("firstpg.html")
 
 @app.route("/login")
 def Login():
     return render_template("login.html")
-
-@app.route("/")
-def Intermediary():
-    return render_template("firstpg.html")
 
 @app.route("/home")
 def Home():
@@ -21,5 +20,14 @@ def Home():
 @app.route("/register")
 def Register():
     return render_template("register.html")
+
+@app.route("/list")
+def ExerciseList():
+    return render_template("list.html")
+
+@app.route("/logout")
+def Logout():
+    session.clear()
+    return redirect("/")
 
 app.run(debug=True, port=5000)
